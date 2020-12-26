8793fc844574791ecc19e96128149942364b67fa
swetha1917 <76477056+swetha1917@users.noreply.github.com>
2020-12-26 13:18:16 +0530

    created a loop pattern
    displays a loop pattern of 01

---
+++ challenge3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+	//1. Loop variable
+	//2.condition
+	int i=0;
+	int j=0;
+	for(i=1;i<=6;i++)
+	{
+		// Loop body
+		
+		for(j=1;j<=i;j++)
+		{
+			printf("01");
+		}
+	printf("\n");	
+	}
+}
